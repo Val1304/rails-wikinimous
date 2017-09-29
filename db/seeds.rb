b6412e368d73ee8d1ca883c946449084bbc437d2
Valentin Le Blan <valentinleblan@gmail.com>
2017-09-29 16:21:20 +0200

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 10 fake Articles...'
+10.times do
+  article = Article.new(
+    title:    Faker::Name.name,
+    content: Faker::Beer.name,
+  )
+  article.save!
+end
+puts 'Finished!'
